19f374a9cff1624fcd3d37ac61c66f2216fb8479
Larissa Diniz <larissa.dtm@gmail.com>
2019-12-30 16:16:48 -0300

    show score

--- SwiftUIKit/SwiftUIKit/ViewController.swift
+++ SwiftUIKit/SwiftUIKit/ViewController.swift
@@ -12,9 +12,11 @@ class ViewController: UIViewController {
     
     var currentValue: Int = 50
     var targetValue: Int = 0
+    var score: Int = 0
     
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +36,8 @@ class ViewController: UIViewController {
         }
         
         let points = 100 - difference
+        
+        score += points
         
         let message = "The value of the slider is now: \(currentValue)" + "\nThe target value is: \(targetValue)" + "\nThe diffenrece is: \(difference)" + "\nYou scored \(points) points "
         
@@ -58,10 +62,15 @@ class ViewController: UIViewController {
         currentValue = 50
         slider.value = Float(currentValue)
         updateLabels()
+        updateScore()
     }
     
     func updateLabels() {
         targetLabel.text = String(targetValue)
+    }
+    
+    func updateScore() {
+        scoreLabel.text = String(score)
     }
 }
 
