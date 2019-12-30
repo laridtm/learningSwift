761189d781e86c39bab9a946c9c2c79c77d6e31c
Larissa Diniz <larissa.dtm@gmail.com>
2019-12-30 17:31:48 -0300

    fixing new round

--- SwiftUIKit/SwiftUIKit/ViewController.swift
+++ SwiftUIKit/SwiftUIKit/ViewController.swift
@@ -65,13 +65,15 @@ class ViewController: UIViewController {
         
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
 
-        let action = UIAlertAction(title: "OK", style: .default , handler: nil)
+        let action = UIAlertAction(title: "OK", style: .default , handler: {
+            action in
+            self.startNewRound()
+        })
 
         alert.addAction(action)
 
         present(alert, animated: true, completion: nil)
         
-        startNewRound()
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
