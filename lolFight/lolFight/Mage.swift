dca96eb0cd3060c5a9838d2eab4619239a75571c
Larissa Diniz <larissa.dtm@gmail.com>
2020-01-12 18:47:04 -0300

    add class mage

---
+++ lolFight/lolFight/Mage.swift
@@ -0,0 +1,47 @@
+//
+//  Mage.swift
+//  lolFight
+//
+//  Created by Larissa Diniz  on 10/01/20.
+//  Copyright © 2020 Larissa Diniz . All rights reserved.
+//
+
+import Foundation
+
+class Mage: Champion {
+    var name: String
+    var alive: Bool
+    var classe: String
+    var base: Bool
+    var kills: Int
+    var deaths: Int
+    var mana: Int
+    
+    init(name: String, alive: Bool, classe: String, base: Bool, kills: Int, deaths: Int, mana: Int) {
+        self.name = name
+        self.alive = alive
+        self.classe = classe
+        self.base = base
+        self.kills = kills
+        self.deaths = deaths
+        self.mana = mana
+    }
+    
+    func die() {
+        alive = false
+        base = true
+    }
+    
+    func toLive() {
+        base = false
+        alive = true
+        mana = 100
+    }
+    
+    func fight() {
+        
+    }
+    
+    
+    
+}
