816fc7131773950a5006a4181e8e232816949f11
Larissa Diniz <larissa.dtm@gmail.com>
2020-01-15 11:34:59 -0300

    creating lolFight2

---
+++ lolFight2/lolFight2/Champion.swift
@@ -0,0 +1,20 @@
+//
+//  Champion.swift
+//  lolFight2
+//
+//  Created by Larissa Diniz  on 15/01/20.
+//  Copyright © 2020 Larissa Diniz . All rights reserved.
+//
+
+import Foundation
+
+protocol Champion {
+    var name: String {get set}
+    var life: Int {get set}
+    var kill: Int {get set}
+    var die: Int {get set}
+    
+    func attack()
+    func deffense()
+    
+}
