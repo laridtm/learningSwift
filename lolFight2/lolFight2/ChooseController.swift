100392edbcaaa0a3cc9ea9ee8b9791c59561d8b8
Larissa <>
2020-01-24 11:54:03 -0300

    right champions

--- lolFight2/lolFight2/ChooseController.swift
+++ lolFight2/lolFight2/ChooseController.swift
@@ -13,7 +13,9 @@ class ChooseController: UIViewController {
     @IBOutlet weak var champion1Image: UIImageView!
     @IBOutlet weak var champion2Image: UIImageView!
     
-    var champion1Winner : Bool = true
+    var champion1Winner : Bool = false
+    var boot : Champion?
+    var myChampion : Champion?
     
     let lux = Mage(name: "Lux", life: 100, kills: 0, deaths: 0)
     let garen = Tank(name: "Garen", life: 100, kills: 0, deaths: 0)
@@ -22,22 +24,26 @@ class ChooseController: UIViewController {
     
     @IBAction func Lux(_ sender: UIButton) {
         champion1Image.image = UIImage(named: "lux3")
+        myChampion = lux
     }
     
     @IBAction func Taric(_ sender: UIButton) {
         champion1Image.image = UIImage(named: "taric")
+        myChampion = taric
     }
     
     @IBAction func Garen(_ sender: UIButton) {
         champion1Image.image = UIImage(named: "garen4")
+        myChampion = garen
     }
     
     @IBAction func Missfortune(_ sender: UIButton) {
         champion1Image.image = UIImage(named: "missFortune2")
+        myChampion = missFortune
     }
     
     @IBAction func Attack(_ sender: UIButton) {
-        let fight1 = Fight.init(champion1: lux, champion2: garen)
+        let fight1 = Fight.init(champion1: myChampion!, champion2: boot!)
         champion1Winner = fight1.fight()
     }
     
@@ -53,10 +59,6 @@ class ChooseController: UIViewController {
         }
     }
     
-//    func prepare(for segue: "ShowResultSegue", sender: Any?) {
-//        let result = segue.destination as? ShowResultController
-//    }
-//
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -64,28 +66,18 @@ class ChooseController: UIViewController {
         
         if random == 0 {
             champion2Image.image = UIImage(named: "lux3")
+            boot = lux
         } else if random == 1 {
             champion2Image.image = UIImage(named: "taric")
+            boot = taric
         } else if random == 2 {
             champion2Image.image = UIImage(named: "garen4")
+            boot = garen
         } else if random == 3 {
             champion2Image.image = UIImage(named: "missFortune2")
+            boot = missFortune
         }
              
-          
-        
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
